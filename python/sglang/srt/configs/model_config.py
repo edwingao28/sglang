71e146f279cd0372bcd950ffb33d575a9e69e786
luoyuan.luo <luoyuan.luo@antgroup.com>
2026-01-19 14:09:52 +0800

    Support Eagle2_5_VL

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -1326,6 +1326,7 @@ multimodal_model_archs = [
     "DotsVLMForCausalLM",
     "DotsOCRForCausalLM",
     "Sarashina2VisionForCausalLM",
+    "Eagle2_5_VLForConditionalGeneration",
     "NVILAForConditionalGeneration",
     "NVILALiteForConditionalGeneration",
     "DeepseekOCRForCausalLM",
